b0c296aeea2c5c36afbc42e18e0e91d7280e5a85
roxy003 <monalshah03@gmail.com>
2023-01-27 00:32:04 +0530

    New commit

---
+++ vid1_Introduction.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+
+function addThreeNumbers(a, b, c) {
+  return a + b + c;
+}
+let d = addThreeNumbers(20, 68, 59);
+console.log(d);
---
+++ vid2_Variables.js
@@ -0,0 +1,18 @@
+console.log("Vid2_Variables");
+
+let a = 67;
+//console.log(a);
+
+a = "Harry";
+console.log(
+  a
+); /*When using let you can change(update) the data/value of the identifier(here is a) but you
+cannot redeclare it.
+*/
+
+/*Rules for Choosing Variable Names in Js
+1  letters, digits, underscores and $ sign allowed.
+2  Name must begin with a letter, _ or $ sign.
+3  Js reserved words cannot be used as variable names.
+4  Is case sensitive.
+*/
