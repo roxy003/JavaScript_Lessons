6f9c784766e38e6794400b27d0c28aed72b1af54
roxy003 <monalshah03@gmail.com>
2023-01-27 03:07:37 +0530

    A new commit

---
+++ vid6_Operators and Expressions.js
@@ -0,0 +1,35 @@
+//  Operators in Js
+
+console.log("Operators in Js");
+
+let a = 45;
+let b = 4;
+
+console.log("a + b =", a + b); //Addition
+console.log("a - b =", a - b); //Subtraction
+console.log("a / b =", a / b); //Division
+console.log("a * b =", a * b); //Multiplication
+console.log("a ** b =", a ** b); // means a to the power b so here it becomes 45*45*45*45 (because a**b)
+console.log("a % b =", a % b); //shows remainder
+console.log("a++ =", a++);
+console.log("a-- =", a--);
+console.log("--a =", --a);
+console.log("a =", a);
+console.log("a++ =", a++);
+
+//Assignment operators
+
+let assignment = 1;
+assignment += 5; //same assignment as assignment = assignment + 5
+console.log(assignment);
+
+assignment -= 5; //same assignment as assignment = assignment - 5
+console.log(assignment);
+
+assignment *= 5; //same assignment as assignment = assignment * 5
+console.log(assignment);
+
+assignment /= 5; //same assignment as assignment = assignment / 5
+console.log(assignment);
+
+//Comparison Operators
