7fa0958f47acae96af5baa8b3fd275ee754ce91e
roxy003 <monalshah03@gmail.com>
2023-01-27 19:22:52 +0530

    new commit

--- vid6_Operators and Expressions.js
+++ vid6_Operators and Expressions.js
@@ -33,3 +33,24 @@ assignment /= 5; //same assignment as assignment = assignment / 5
 console.log(assignment);
 
 //Comparison Operators
+
+let comp1 = 6;
+let comp2 = "6";
+let comp3 = 7;
+
+console.log("comp1 == comp2 is", comp1 == comp2); // checks only value
+console.log("comp1 != comp2 is", comp1 != comp2);
+console.log("comp1 === comp2 is", comp1 === comp2); //Result would be false because === checks value and its type as well.
+console.log("comp1 !== comp2 is", comp1 !== comp2); //checks both value and its type
+console.log("comp1 > comp2 is", comp1 > comp3);
+
+//Logical Operators
+let x = 5;
+let y = 6;
+
+console.log(x < y && x == 5);
+console.log(x > y || x == 5);
+console.log(!false);
+console.log(!true);
+
+// there is also bitwise operators we will learn about it in future
