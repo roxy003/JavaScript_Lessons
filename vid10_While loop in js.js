5717f9f2b8f12d498a12e3ed3959ff4f3a58260e
roxy003 <monalshah03@gmail.com>
2023-01-28 23:18:03 +0530

    a new commit

---
+++ vid10_While loop in js.js
@@ -0,0 +1,65 @@
+/*
+While loop executes code based on condition.
+  NOTE:  if the condition never becomes false, the loop will never end and this
+  might crash the runtime.
+  In while loop first condition is checked then block runs.
+*/
+
+/*
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+
+let i = 0;
+while (i < n) {
+  console.log(i);
+  i++;
+}
+*/
+//Practice while loop
+
+/*
+let name1 = prompt("Enter name");
+let i = 1;
+while (i <= name1.length) {
+  console.log(i);
+  i++;
+} // I know thia programm doesn't make sense .. But i could not think of anything
+*/
+
+/*
+//====== Do Whilr loop=======
+   do while loop is variant of while loop.
+   In do while loop first block runs and then the condition is checked..
+
+   
+*/
+
+/*   let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < n);
+//Do while loop is atleast executed once even if the condition gets false..
+*/
+
+//Practice of do while loop
+let animalnames = {
+  zibra: "aged 10",
+  Swan: "aged 5",
+  tiger: "aged 9",
+  Dolphin: "aged 10",
+  elephant: "aged 20",
+};
+
+//let aninames = prompt("Enter name of an animal");
+/*let i = 4;
+do {
+  console.log(animalnames[1]);
+  i++;
+} while (animalnames.length > i);
+
+Did'nt work. How to call an object in do while loop.. Find out..
+*/
